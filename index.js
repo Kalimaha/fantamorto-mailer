c7e771a82eff0907c33e51b20d68d994fb0ac892
Guido Barbaglia <guido.barbaglia@rea-group.com>
2017-02-16 15:49:17 +1100

    WIP

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
-var express = require('express')
-var app = express()
+const express     = require('express')
+const app         = express()
+const nodemailer  = require('nodemailer')
+
+const transporter = nodemailer.createTransport({
+  service: 'Gmail',
+  auth: {
+    user: 'fantamorto.online@gmail.com',
+    pass: 'Fantamorto2017'
+  }
+})
+const from = 'fantamorto.online@gmail.com'
+const to = 'guido.barbaglia@gmail.com'
+
 
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  // res.send('Hello World!')
+  const text = '<h1>Hello world from</h1>';
+  const mailOptions = {
+    from: from,
+    to: to,
+    subject: 'Email Example',
+    html: text
+  }
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error)  { res.send(error) }
+    else        { res.send('Message sent.') }
+  })
 })
 
 app.listen(app.get('port'), function() {
