51f5c515c8dbe2e151bd3e6da65b24a89a8f0516
Guido Barbaglia <guido.barbaglia@rea-group.com>
2017-02-17 17:00:37 +1100

    WIP

--- index.js
+++ index.js
@@ -2,15 +2,36 @@ const express     = require('express')
 const app         = express()
 const path        = require('path');
 const mailer      = require(path.resolve(__dirname, './mailer.js'))
+const db_client   = require(path.resolve(__dirname, './db.js'))
 
 app.set('port', (process.env.PORT || 5000));
 
-app.get('/', (req, res) => {
-  mailer.send('guido.barbaglia@gmail.com')
-    .then(() => res.sendStatus(200))
-    .catch((err) => res.send(err))
+app.get('/request/:id', (req, res) => {
+  db_client.connection().ref(`users/${req.params.id}`).on('value', (snapshot) => {
+    const user = snapshot.val()
+
+    if (user != null && user.status == 'PENDING') {
+      mailer.sendForApproval(user.id, user.name)
+        .then(() => res.sendStatus(200))
+        .catch((err) => res.send(err))
+    } else {
+      res.sendStatus(200)
+    }
+  })
 })
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+app.get('/approve/:id', (req, res) => {
+  db_client.connection().ref(`users/${req.params.id}`).on('value', (snapshot) => {
+    const user = snapshot.val()
+    user.status = 'APPROVED'
+    db_client.connection().ref(`users/${req.params.id}`).update(user).then(() => {
+      mailer.sendApproved(user.email)
+        .then(() => res.sendStatus(200))
+        .catch((err) => res.send(err))
+    })
+  })
+})
+
+app.listen(app.get('port'), () => {
+  console.log('Express running on port', app.get('port'));
 });
--- mailer.js
+++ mailer.js
@@ -1,11 +1,25 @@
 const nodemailer  = require('nodemailer-promise')
 
-const text = () => `
+const ADMIN       = 'guido.barbaglia@gmail.com'
+// const BASE_URL    = 'https://fantamorto-mailer.herokuapp.com'
+const BASE_URL    = 'http://localhost:5000'
+
+const approvedText = () => `
   <h1>F A N T A M O R T O</h1>
   <hr>
   Benvenuto! Il tuo account è stato autorizzato.
   Accedi a <a target="_blank" href="fantamorto-4f1ad.firebaseapp.com">Fantamorto</a> per
   iniziare a giocare!
+`
+
+const forApprovalText = (id, name) => `
+  <h1>F A N T A M O R T O</h1>
+  <hr>
+  L'utente ${name} ha richiesto di iscriversi a FANTAMORTO.
+  <br><br><br>
+  <a target="_blank" href="${BASE_URL}/approve/${id}">
+    Approva Richiesta
+  </a>
 `
 
 const sendEmail = nodemailer.config({
@@ -14,15 +28,28 @@ const sendEmail = nodemailer.config({
   server: 'smtp.gmail.com'
 });
 
-const options = (to) => {
+const approvedOptions = (to) => {
   return {
     subject: 'Benvenuto a FANTAMORTO!',
     senderName: 'FANTAMORTO',
     receiver: to,
-    html: text()
+    html: approvedText()
   }
 }
 
-const send = (to) => sendEmail(options(to))
+const forApprovalOptions = (id, name) => {
+  return {
+    subject: 'Nuova Richiesta di Iscrizione',
+    senderName: 'FANTAMORTO',
+    receiver: ADMIN,
+    html: forApprovalText(id, name)
+  }
+}
 
-module.exports = { send: send }
+const sendApproved = (to) => sendEmail(approvedOptions(to))
+const sendForApproval = (id, name) => sendEmail(forApprovalOptions(id, name))
+
+module.exports = {
+  sendApproved: sendApproved,
+  sendForApproval: sendForApproval
+}
